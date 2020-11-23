23f91bd13319cb447e33cab7889b68b7ff371c0e
cmertenok <artempozitiv56@gmail.com>
2020-11-24 00:11:45 +0200

    Create LAB1.js

---
+++ LAB1.js
@@ -0,0 +1,16 @@
+'use strict';
+
+function MyName(name) {
+    console.log(`My name is ${name}!`);
+}
+MyName('Artem');
+
+function MyAge(age) {
+    console.log(`Im ${age} years old!`);
+}    
+MyAge(17);
+
+function MyCountry(country){
+    console.log(`Im from ${country}!`);
+}
+MyCountry('Ukraine');
